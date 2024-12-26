ce7795f2591c0ab0076e4f5732b45c2461902540
70ni <4thedevelopments@gmail.com>
2024-12-26 12:02:08 +0530

    other changes

---
+++ app/(root)/chat.tsx
@@ -0,0 +1,12 @@
+import {Text,View} from "react-native"
+// import { SafeAreaView } from "react-native-web";
+
+const Chat =()=>{
+    return (
+        <View>
+            <Text>Chat</Text>
+        </View>
+    )
+}
+
+export default Chat;
---
+++ app/(root)/home.tsx
@@ -0,0 +1,12 @@
+import {Text,View} from "react-native"
+// import { SafeAreaView } from "react-native-web";
+
+const Home =()=>{
+    return (
+        <View className="flex items-center justify-center">
+            <Text className="text-red-600">Home</Text>
+        </View>
+    )
+}
+
+export default Home;
